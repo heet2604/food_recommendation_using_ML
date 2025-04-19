f33fae2f062c7d83f7f3481301d4afd36c57f140
heet2604 <heetdudhwala848@gmail.com>
2025-04-19 12:26:57 +0530

    cors edit

--- backend/server.js
+++ backend/server.js
@@ -26,11 +26,12 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://food-recommendation-using-ml.vercel.app", // Allow requests from your frontend
-    credentials: true, // Allow cookies/sessions with requests
+    origin: "https://food-recommendation-using-ml.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
-app.use(express.urlencoded({ extended: true }));
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
