9d215b4814fc7e084b430b7644953f305eff9307
heet2604 <heetdudhwala848@gmail.com>
2025-04-19 16:35:07 +0530

    Revert "home end point"
    This reverts commit 6fceca3754720b6ffed62ba50e0ea585f30d98e8.

--- frontend/src/Home/home.jsx
+++ frontend/src/Home/home.jsx
@@ -141,7 +141,7 @@ export default function DashboardHome() {
 
     const fetchDashboardData = async () => {
       try {
-        const response = await axios.get("https://food-recommendation-using-ml-1.onrender.com/api/dashboard-data", {
+        const response = await axios.get("http://localhost:5000/api/dashboard-data", {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
           },
