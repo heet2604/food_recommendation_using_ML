78796cb979436401ddf1d60ed42d0e57d382b709
heet2604 <heetdudhwala848@gmail.com>
2025-04-19 16:35:06 +0530

    Revert "new ocr update"
    This reverts commit 4732a65ca3d58327c99f5851cb1f449c1c758841.

--- backend/ocr_server.py
+++ backend/ocr_server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 # Enable CORS if calling from another server
 from flask_cors import CORS
-CORS(app, resources={r"/ocr": {"origins": "*"}})
+CORS(app)
 
 ocr = PaddleOCR(use_angle_cls=True, lang="en")  # English OCR Model
 
