0ae2556b2976f89d3ee1c87a30a7e84cd2839a4f
heet2604 <heetdudhwala848@gmail.com>
2025-05-01 12:53:17 +0530

    whitelist

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,7 @@ app.use((req, res, next) => {
 const whitelist = [
   'http://localhost:3000',
   'https://food-recommendation-using-ml.vercel.app',
+  'https://food-recommendation-using-ml.onrender.com'
 ];
 
 app.use(cors({
